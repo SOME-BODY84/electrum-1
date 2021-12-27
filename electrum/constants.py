85f2874d3e319d62521c325c95f1f4981a4710f9
SOME-BODY84 <86975353+SOME-BODY84@users.noreply.github.com>
2021-12-27 15:16:06 +0100

    Update constants.py

--- electrum/constants.py
+++ electrum/constants.py
@@ -47,11 +47,3 @@ networks = {
 }
 
 net = networks['Bitcoin']
-
-def select_network(network='Bitcoin'):
-    if not network in networks:
-        raise Exception('Invalid Network. Available: {}'.format(
-            list(networks.keys())))
-    global net
-    net = networks.get(network, 'Bitcoin')
-    return
