0bc8269b4058a3fab678e911220411a2e9959f79
SOME-BODY84 <86975353+SOME-BODY84@users.noreply.github.com>
2021-12-27 21:32:55 +0100

    Update pruxcoin_mainnet.py

--- electrum/networks/pruxcoin_mainnet.py
+++ electrum/networks/pruxcoin_mainnet.py
@@ -98,10 +98,10 @@ class PruxcoinMainnet(AbstractNet, AuxPowMixin):
         bits = last.get('bits')
         target = blockchain.bits_to_target(bits)
         
-        if index > 15000:
-        nActualTimespan = last.get('timestamp') - first.get('timestamp') / 2
+        if index < 15000:
+             nActualTimespan = last.get('timestamp') - first.get('timestamp') / 2
         else:
-        nActualTimespan = last.get('timestamp') - first.get('timestamp')         
+        nActualTimespan = last.get('timestamp') - first.get('timestamp')   
   
         nActualTimespan = max(nActualTimespan, cls.TARGET_TIMESPAN // 4)
         nActualTimespan = min(nActualTimespan, cls.TARGET_TIMESPAN * 4)
