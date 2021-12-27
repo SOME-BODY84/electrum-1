18ad9bfa63daee755b4c42983a7ce144bbd6e9d3
SOME-BODY84 <86975353+SOME-BODY84@users.noreply.github.com>
2021-12-27 15:01:03 +0100

    Update auxpow_mixin2.py

--- electrum/networks/auxpow_mixin2.py
+++ electrum/networks/auxpow_mixin2.py
@@ -1,1 +1,10 @@
+class AuxPowMixin(object):
+    AUXPOW_START_HEIGHT = 0
+    AUXPOW_CHAIN_ID = 0x03BF
+    BLOCK_VERSION_AUXPOW_BIT = 0
 
+    @classmethod
+    def is_auxpow_active(cls, header) -> bool:
+        height_allows_auxpow = header['block_height'] >= cls.AUXPOW_START_HEIGHT
+        version_allows_auxpow = header['version'] & cls.BLOCK_VERSION_AUXPOW_BIT
+return height_allows_auxpow and version_allows_auxpow 
