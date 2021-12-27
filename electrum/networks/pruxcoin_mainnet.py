57b49ce154370d5eefd5abea1fdc4035535f98f5
SOME-BODY84 <86975353+SOME-BODY84@users.noreply.github.com>
2021-12-28 00:10:00 +0100

    Update pruxcoin_mainnet.py

--- electrum/networks/pruxcoin_mainnet.py
+++ electrum/networks/pruxcoin_mainnet.py
@@ -37,44 +37,40 @@ class PruxcoinMainnet(AbstractNet, AuxPowMixin):
         'p2wsh':       0x02aa7ed3,  # Zpub
     }
     XPUB_HEADERS_INV = inv_dict(XPUB_HEADERS)
-    BIP44_COIN_TYPE = 0
+    BIP44_COIN_TYPE = 7
     LN_REALM_BYTE = 0
     LN_DNS_SEEDS = []
     OPEN_ALIAS_PREFIX = 'prux'
-    PAYMENT_URI_SCHEME = 'prux'
+    PAYMENT_URI_SCHEME = 'pruxcoin'
     PAYMENT_REQUEST_PKI_TYPE = "dnssec+prux"
     APPLICATION_PAYMENT_REQUEST_TYPE = 'application/pruxcoin-paymentrequest'
     APPLICATION_PAYMENT_TYPE = 'application/pruxcoin-payment'
     APPLICATION_PAYMENT_ACK_TYPE = 'application/pruxcoin-paymentack'
-    COINBASE_MATURITY = 259
+    COINBASE_MATURITY = 100
     COIN = 100000000
-    TOTAL_COIN_SUPPLY_LIMIT = 114375
+    TOTAL_COIN_SUPPLY_LIMIT = 21000000
     SIGNED_MESSAGE_PREFIX = b"\x18Pruxcoin Signed Message:\n"
 
-    BASE_UNITS = {'PRUX': 8, 'mPRUX': 5, 'uPRUX': 2, 'swartz': 0}
+    BASE_UNITS = {'PRUX': 8, 'mPRUX': 5, 'uPRUX': 2, 'sat': 0}
     BASE_UNITS_INVERSE = inv_dict(BASE_UNITS)
-    BASE_UNITS_LIST = ['PRUX', 'mPRUX', 'uPRUX', 'swartz']
+    BASE_UNITS_LIST = ['PRUX', 'mPRUX', 'uPRUX', 'sat']
     DECIMAL_POINT_DEFAULT = 8  # mPRUX
     AUXPOW_CHAIN_ID = 0x03BF
     AUXPOW_START_HEIGHT = 15615201
     BLOCK_VERSION_AUXPOW_BIT = 0x100
 
-    BLOCK_EXPLORERS = {}
+    BLOCK_EXPLORERS = { }
 
     # The default Bitcoin frame size limit of 1 MB doesn't work for AuxPoW-based
     # chains, because those chains' block headers have extra AuxPoW data.  A limit
     # of 10 MB works fine for Namecoin as of block height 418744 (5 MB fails after
     # height 155232); we set a limit of 20 MB so that we have extra wiggle room.
     MAX_INCOMING_MSG_SIZE = 20_000_000  # in bytes
+
     TARGET_TIMESPAN = int(60 * 6)
     TARGET_SPACING = int(3)
     INTERVAL = int(TARGET_TIMESPAN / TARGET_SPACING)
 
-    
-    HEADER_SIZE = 80  # bytes
-    MAX_TARGET = 0x00000fffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
-
-    
     @classmethod
     def get_target(cls, height: int, blockchain) -> int:
         index = height // 2016 - 1
@@ -94,7 +90,7 @@ class PruxcoinMainnet(AbstractNet, AuxPowMixin):
             return blockchain.bits_to_target(last['bits'])
 
         # new target
-        if (index * 2016 + 2015 > 15615201) and (index * 2016 + 2015 + 1 > 2016):
+        if (index * 2016 + 2015 > 19200) and (index * 2016 + 2015 + 1 > 2016):
             # Namecoin: Apply retargeting hardfork after AuxPoW start
             first = blockchain.read_header(height - cls.INTERVAL - 1)
         else:
@@ -106,15 +102,10 @@ class PruxcoinMainnet(AbstractNet, AuxPowMixin):
 
         bits = last.get('bits')
         target = blockchain.bits_to_target(bits)
-        if (height > 15000):
-            nActualTimespan = (last.get('timestamp') - first.get('timestamp') // 2) 
-        else:
-            nActualTimespan = last.get('timestamp') - first.get('timestamp')
+        nActualTimespan = last.get('timestamp') - first.get('timestamp')
         nActualTimespan = max(nActualTimespan, cls.TARGET_TIMESPAN // 4)
         nActualTimespan = min(nActualTimespan, cls.TARGET_TIMESPAN * 4)
         new_target = min(cls.MAX_TARGET, (target * nActualTimespan) // cls.TARGET_TIMESPAN)
-        
-        
         # not any target can be represented in 32 bits:
         new_target = blockchain.bits_to_target(blockchain.target_to_bits(new_target))
-        return new_target 
+        return new_target
