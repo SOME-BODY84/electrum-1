af52a3fa233b3ada1ebc57212d7bebeaff805980
SOME-BODY84 <86975353+SOME-BODY84@users.noreply.github.com>
2021-12-27 16:58:30 +0100

    Update pruxcoin_mainnet.py

--- electrum/networks/pruxcoin_mainnet.py
+++ electrum/networks/pruxcoin_mainnet.py
@@ -84,15 +84,15 @@ class PruxcoinMainnet(AbstractNet, AuxPowMixin):
         index = height // 2016 - 1
 
 	
-     if (index >= 7770000) 
-     TARGET_TIMESPAN = int(15 * 60); 
-     TARGET_SPACING = int(9); 
-     else if (index >= 7331700) 
-     TARGET_TIMESPAN = int(5 * 60 * 60); 
-     TARGET_SPACING = int(9); 
+     if index >= 7770000:
+     TARGET_TIMESPAN = int(15 * 60)
+     TARGET_SPACING = int(9)
+     else if index >= 7331700:
+     TARGET_TIMESPAN = int(5 * 60 * 60)
+     TARGET_SPACING = int(9)
      else 
-     TARGET_TIMESPAN = int(60 * 6);
-     TARGET_SPACING = int(3); 
+     TARGET_TIMESPAN = int(60 * 6)
+     TARGET_SPACING = int(3)
     
 
 
