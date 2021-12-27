a2a10fbdddf7f5f320d5e67d4181813df437871d
SOME-BODY84 <86975353+SOME-BODY84@users.noreply.github.com>
2021-12-27 16:43:32 +0100

    Update pruxcoin_mainnet.py

--- electrum/networks/pruxcoin_mainnet.py
+++ electrum/networks/pruxcoin_mainnet.py
@@ -90,7 +90,7 @@ class PruxcoinMainnet(AbstractNet, AuxPowMixin):
             return blockchain.bits_to_target(last['bits'])
 
         # new target
-        if (index * 2016 + 2015 > 19200) and (index * 2016 + 2015 + 1 > 2016):
+        if (index * 2016 + 2015 > 15615201) and (index * 2016 + 2015 + 1 > 2016):
             # Namecoin: Apply retargeting hardfork after AuxPoW start
             first = blockchain.read_header(height - cls.INTERVAL - 1)
         else:
