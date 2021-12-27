f8a748512c82169baf9e2f5d702e681067ee9ff7
SOME-BODY84 <86975353+SOME-BODY84@users.noreply.github.com>
2021-12-27 22:26:07 +0100

    Update blockchain.py

--- electrum/blockchain.py
+++ electrum/blockchain.py
@@ -530,19 +530,11 @@ class Blockchain(Logger):
         return False
 
     def get_hash(self, height: int) -> str:
-        def is_height_checkpoint():
-            within_cp_range = height <= constants.net.max_checkpoint()
-            at_chunk_boundary = (height+1) % 2016 == 0
-            return within_cp_range and at_chunk_boundary
 
         if height == -1:
             return '0000000000000000000000000000000000000000000000000000000000000000'
         elif height == 0:
             return constants.net.GENESIS
-        elif is_height_checkpoint():
-            index = height // 2016
-            h, t = self.checkpoints[index]
-            return h
         else:
             header = self.read_header(height)
             if header is None:
