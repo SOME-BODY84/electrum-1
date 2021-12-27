a0d103eb1daf061d336b1bbd9ab2e8a6fd616c2d
SOME-BODY84 <86975353+SOME-BODY84@users.noreply.github.com>
2021-12-27 15:28:02 +0100

    Update constants.py

--- electrum/constants.py
+++ electrum/constants.py
@@ -49,13 +49,6 @@ networks = {
 net = networks['Bitcoin']
 
 def set_mainnet():
-global net
-net = BitcoinMainnet
-
-def select_network(network='Bitcoin'):
-    if not network in networks:
-        raise Exception('Invalid Network. Available: {}'.format(
-            list(networks.keys())))
-    global net
-    net = networks.get(network, 'Bitcoin')
-    return
+     global net
+     net = BitcoinMainnet
+     return
